7f0015342d0d1cebeff7a7d26f124c712a3b05d7
tiago-rodrigues1 <tiagords127@gmail.com>
2022-01-02 20:50:23 -0300

    feat: add charadas-no-escuro

---
+++ charadas-no-escuro/index.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    cout << fixed << setprecision(2);
+
+    double r;
+
+    cin >> r;
+
+    cout << (r * 3.14 * 2) << endl;
+
+    return 0;
+}
