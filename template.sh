a1cb48eedb0ab7d3f53a7654953c9c0ec3e6bbd7
tiago-rodrigues1 <tiagords127@gmail.com>
2022-01-08 20:23:20 -0300

    refactor: remove link padrão

--- template.sh
+++ template.sh
@@ -5,16 +5,10 @@ read DIR_NAME
 
 echo ""
 
-echo "ID da questão: "
-read ID
-
 mkdir "$DIR_NAME"
 touch "$DIR_NAME/index.cpp"
 touch "$DIR_NAME/readme.md"
 
-LINK=$"https://www.beecrowd.com.br/judge/pt/problems/view/$ID"
-
 echo "# Nome da questão" >> "$DIR_NAME/readme.md"
 echo "### Link da questão 🔗" >> "$DIR_NAME/readme.md"
 echo "" >> "$DIR_NAME/readme.md"
-echo "- $LINK" >> "$DIR_NAME/readme.md"
