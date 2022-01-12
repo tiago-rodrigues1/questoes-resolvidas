d1f2977609fd804abb94d4c4ac313e40a761f705
tiago-rodrigues1 <tiagords127@gmail.com>
2022-01-12 18:56:18 -0300

    feat: add setup p index.cpp no script

--- template.sh
+++ template.sh
@@ -11,4 +11,12 @@ touch "$DIR_NAME/readme.md"
 
 echo "# Nome da questão" >> "$DIR_NAME/readme.md"
 echo "### Link da questão 🔗" >> "$DIR_NAME/readme.md"
-echo "" >> "$DIR_NAME/readme.md"
+echo "- " >> "$DIR_NAME/readme.md"
+
+echo "#include <bits/stdc++.h>" >> "$DIR_NAME/index.cpp"
+echo "" >> "$DIR_NAME/index.cpp"
+echo "using namespace std;" >> "$DIR_NAME/index.cpp"
+echo "" >> "$DIR_NAME/index.cpp"
+echo "int main() {" >> "$DIR_NAME/index.cpp"
+echo "return 0;" >> "$DIR_NAME/index.cpp"
+echo "}" >> "$DIR_NAME/index.cpp"
