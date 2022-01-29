376983542a69d2a542e660e385c6233bc5dad192
tiago-rodrigues1 <tiagords127@gmail.com>
2022-01-29 15:14:28 -0300

    feat: add questao irmaos

---
+++ irmaos/index.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+
+    cin >> n >> m;
+
+    int dif = m - n;
+    int older = m + dif;
+
+    cout << older << "\n";
+
+    return 0;
+}
